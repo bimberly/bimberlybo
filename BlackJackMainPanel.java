994cf2b38d375161eb62ff80f29f030389c273b6
bimberly <46360762+bimberly@users.noreply.github.com>
2019-01-20 14:13:03 -0500

    Update BlackJackMainPanel.java

--- BlackJackMainPanel.java
+++ BlackJackMainPanel.java
@@ -1,9 +1,19 @@
 //imports
-import javax.swing.*;
-import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 
 class BlackJackMainPanel extends JPanel implements ActionListener {
 
@@ -24,62 +34,68 @@ class BlackJackMainPanel extends JPanel implements ActionListener {
 	private void createAndShowGUI() {
 		setLayout(new FlowLayout());
 		JFrame frame = new JFrame("Blackjack");
-		JPanel contentPane = new JPanel(new BorderLayout());
-		JLabel welcome = new JLabel("Welcome to Blackjack!");
-		contentPane.add(welcome, BorderLayout.NORTH);
-		contentPane.setVisible(true);
+
+		try {
+			ImageIcon cards =  new ImageIcon(ImageIO.read(getClass().getResource("Pictures/blackjackcards.gif")));
+			JLabel welcome = new JLabel("Welcome to Blackjack!", JLabel.CENTER);
+			welcome.setVerticalTextPosition(JLabel.RIGHT);
+			welcome.setHorizontalTextPosition(JLabel.CENTER);
+			welcome.setIcon(cards);
+		} catch (Exception e) {System.out.println(e);};
+
+
 		//creating buttons
-		
+
 		JButton b1 = new JButton("Leaderboard");
 		b1.setActionCommand("Leaderboard");
 		b1.addActionListener(this);
-		b1.setVisible(true);
-		contentPane.add(b1, BorderLayout.WEST);
-		
+		this.add(b1, BorderLayout.WEST);
+
 		JButton b2 = new JButton("Play");
 		b2.setActionCommand("Play");
 		b2.addActionListener(this);
-		b2.setVisible(true);
-		contentPane.add(b2, BorderLayout.CENTER);
+		this.add(b2, BorderLayout.CENTER);
 
 		JButton b3 = new JButton("How to Play");
 		b3.setActionCommand("How to Play");
 		b3.addActionListener(this);
-		b3.setVisible(true);
-		contentPane.add(b3, BorderLayout.EAST);
-		
-		frame.setSize(300, 100);
+		this.add(b3, BorderLayout.EAST);
+
+		frame.setSize(250, 150);
 		frame.setVisible(true);
 		frame.toFront();
-		frame.add(contentPane);
+		frame.add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	public void actionPerformed(ActionEvent ae) {
 		if(ae.getActionCommand().equals("Play")) {
 			this.setVisible(false);
 			menuVisibility = false;
-		} else if (ae.getActionCommand().equals("Instructions")) {
+		} else if (ae.getActionCommand().equals("How to Play")) {
 			JOptionPane.showMessageDialog(null,"How to play:\r\n" + 
-					"The objective of the game is to have a set of cards \r\n"
-					+ "whose total value beats the opponentís set of cardsí \r\n"
-					+ "total value without going over the value twenty-one. \r\n"
-					+ "At the beginning, you will decide how much you want \r\n"
-					+ "to bet. Then, each player will be dealt two cards, \r\n"
-					+ "one facing up, one facing down. Cards two to ten are \r\n"
-					+ "their actual values, all face cards have a value of ten, \r\n"
-					+ "and ace has a value of either one or eleven, depending \r\n"
-					+ "on what you want. From there, you have an option to ìHitî \r\n"
-					+ "or ìStand.î Hit meaning you would like another card, but \r\n"
-					+ "you do risk being ìBust,î which is when you have gone over \r\n"
-					+ "twenty-one. Stand means you do not want another card. If \r\n"
-					+ "you win the round, then you gain the bet money and if you \r\n"
-					+ "lose, you donít get anything.\r\n" + 
-					"Note: Suits do not matter in Blackjack.\r\n");  
+					"The objective of the game is to have a set of cards whose total \r\n"
+					+ "value beats the opponent‚Äôs set of cards‚Äô total value without \r\n"
+					+ "going over the value twenty-one. Each player bets two dollars \r\n"
+					+ "the beginning of each round. Then, each player will be dealt \r\n"
+					+ "two cards. Cards two to ten are their actual values, all face \r\n"
+					+ "cards have a value of ten, and ace has a value of either one \r\n"
+					+ "or eleven, depending on what you want. When you receive and \r\n"
+					+ "ace, you can press the purple card for the value of eleven \r\n"
+					+ "and the blue card for a value of one. From there, you have an \r\n"
+					+ "option to ‚ÄúHit‚Äù or ‚ÄúStand.‚Äù Hit being the grey card and it \r\n"
+					+ "means you would like another card, but you do risk going over \r\n"
+					+ "twenty-one. Stand being the yellow card and it means you do \r\n"
+					+ "not want another card. If you win the round, then you gain the \r\n"
+					+ "bet money and if you lose, you don‚Äôt get anything. A menu will \r\n"
+					+ "pop up asking you whether or not you want to play another round, \r\n"
+					+ "if you do, simply type in, ‚Äúyes,‚Äù if not, then type, ‚Äúno,‚Äù and \r\n"
+					+ "enter your name to save it to hopefully make the leaderboards.\r\n" + 
+					"Note: Suits do not matter in Blackjack.\r\n");
 		} else if (ae.getActionCommand().equals("Leaderboard")) {
 			String S = openTextFile();
 			JOptionPane.showMessageDialog(null, S);
 		}
-		
+
 	}
 	private String openTextFile() {
 		String S = "These are the top scores:\n";
@@ -93,7 +109,7 @@ class BlackJackMainPanel extends JPanel implements ActionListener {
 		} catch(Exception e) { e.printStackTrace();}
 		return S;
 	}
-	
+
 	public static void main(String[] args) {
 		BlackJackMainPanel menuPanel = new BlackJackMainPanel();
 	}
